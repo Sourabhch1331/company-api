b4389861fd857b1e1d54f17c59d648fbcc97b04b
sourabhch13 <sourabh.c@consultadd.com>
2024-01-14 21:55:43 +0530

    implemented create and get routes for Department

--- company_management/api/models.py
+++ company_management/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Department(models.Model):
+    name=models.CharField(max_length=50, unique=True)
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+
+    description=models.TextField(blank=True)
+
+    def __str__(self):
+        return 'Department name: '+self.name + ' ,Department description: '+self.description
+
+
+class Employee(models.Model):
+    firt_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    email=models.EmailField()
+    role=models.CharField(max_length=20)
+    department=models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    manager_id=models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.firt_name + ' ' + self.last_name
--- company_management/api/views.py
+++ company_management/api/views.py
@@ -1,7 +1,64 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+#models
+from .models import Department, Employee
+
+
 # Create your views here.
 
 
 def index(request):
     return HttpResponse('<h2>Hello!</h2>')
+
+@csrf_exempt
+def departmentHandler(request):
+    if request.method == 'POST':
+        return createDepartment(request)
+    elif request.method == 'GET':
+        return getAllDepartments(request)
+    
+    return JsonResponse(data={'status': 'fail', 'message': 'Not Found'}, status=404)
+
+
+def createDepartment(request):
+    
+    body = json.loads(request.body.decode('utf-8'))
+
+    try:
+        Department.objects.create(name=body['name'], description=body['description'])
+
+    except Exception as e:
+
+        print(e)
+        data={
+            'status': 'fail',
+            'message': 'something went wrong',
+        }
+        return JsonResponse(data, status=400)
+    
+    data={
+        'status': 'success',
+        'message': 'Department created!',
+    }
+    return JsonResponse(data, status=201)
+
+
+def getAllDepartments(request):
+    data={
+        'status': 'success',
+        'message': 'Data Retrived!'
+    }
+    try:
+        allDep=Department.objects.all().values()
+        
+        data['data']=list(allDep)
+    except Exception as e:
+        print(e)
+        data['status']='fail'
+        data['message']='something went wrong'
+
+        return JsonResponse(data,status=400)
+
+    return JsonResponse(data,status=200)
